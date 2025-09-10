19e72db9d712c32559a9d16980df7c432fda23f9
SUJITHA M <sujimanuvel005@gmail.com>
2025-09-10 07:49:51 +0530

    Add files via upload

---
+++ TemperatureConverter.java
@@ -0,0 +1,15 @@
+package task1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TemperatureConverter extends JFrame implements ActionListener{
+    public static void main(String[] args)
+    {
+      JTextField a;
+      JButton b1,b2;
+      JLabel res;
+      J
+    }
+}
